93089f597de38c49d7daeba6e20d1a9b0778a117
grecoluigi <9luigi1@gmail.com>
2022-04-12 17:49:20 +0200

    Initial Commit

---
+++ ScreenCaptureDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ScreenCaptureDemo
+//
+//  Created by Luigi Greco on 12/04/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
